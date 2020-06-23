247aa6485bd8739083a24c98029eb9fb3ed58644
CM-David <CM.David88@yahoo.com>
2020-06-23 15:18:44 -0500

    days work

---
+++ week04/2-Tuesday/labs/JS/script.js
@@ -0,0 +1,171 @@
+
+
+// console.log("Hello world")
+
+// document.body.innerHTML = "Chris"
+
+// console.log("print");
+
+// console.log(object);
+
+// console.log(myName.length);
+
+// var name ="Micah"
+// console.log(name.indexOf("h"))
+
+// var boolean = true
+
+// var not = !true
+
+// var and = boolean && boolean
+
+// var or = !boolean || false
+
+// var equals = 1 != 10
+// console.log(equals)
+
+// if (false || false){
+//     console.log("greater than");
+// }
+// else if (true && true){
+//     console.log("inide and state")
+// }
+// else {
+//     console.log("default statement");
+// }
+
+var fruit = "Papayas"
+// if( fruit == 'Oranges'){
+//     console.log("Oranges");
+// }
+
+// else if(fruit == "Mangos"){
+//     console.log("Mangos");
+// }
+
+// else if( fruit == "Papayas"){
+//     console.log("Papayas");
+// }
+// else{
+//     console.log("Sorry no" + fruit)
+// }
+
+// switch(fruit){
+//     case "Oranges":
+//         console.log("Oranges");
+//         break;
+//     case "Mangos":
+//         console.log("Mangos")
+//     break;
+//     case "Papayas":
+//         console.log("Papayas")
+//     break;
+//     default:
+//         console.log("Default case")
+//     break
+// }
+
+// var day = "Tuesday"
+
+// switch(day){
+//     case "Monday":
+//         console.log("Today is " + day);
+//     break
+//     case "Tuesday":
+//         console.log("Today is " + day);
+//     break
+//     case "Wednesday":
+//         console.log("Today is " + day);
+//     break
+//     case "Thursday":
+//         console.log("Today is " + day);
+//     break
+//     default:
+//         console.log("The apocalypse happened");
+//     break
+// }
+
+
+
+// for(var count = 0; count < 21; count = count + 2){
+    
+//     console.log(count)
+// }
+
+// var red = ("--------------------------")
+// console.log(red);
+
+// var count = 0
+// while (count < 21){
+//     count = count + 2
+//     console.log(count);
+// }
+
+// var myArray = [2, 4, 6, 3, 7, "six", false]
+
+// console.log(myArray.length);
+
+// myArray.push(99)
+
+// console.log(myArray.length);
+
+// myArray[5] = 6
+
+// console.log(myArray[5])
+
+// myArray.pop()
+// console.log(myArray)
+
+// var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+// for(var index = 0; index< days.length; index++){
+//     console.log(days[index])
+// }
+
+
+// while(myArray.length > 0){
+//     console.log(myArray.pop())
+// }
+
+// var lottoNums = [23, 22, 43, 19, 37,16]
+
+// var result = lottoNums.splice(1, 3)
+// var result1 = lottoNums.slice(1, 3)
+
+
+
+// console.log(lottoNums);
+// console.log(result);
+// console.log(result1);
+
+
+// var greeting = "Its about to rain"
+
+// var result = greeting.split(' ')
+// console.log(greeting);
+// console.log(result);
+
+// var name = "name1"
+// var students = {
+//     name1: 'Chris',
+//     name2: 'Michah',
+//     name3: 'Michael',
+//     name4: 'Woody',
+//     name5: 'Dan',
+//     name6: 'Jeremy',
+// }
+
+// console.log(students[name]);
+// var r = students=['name1']
+// var s = students.name1;
+
+// console.log(s);
+
+
+// function hello(name){
+// //     var output = 'Hello ' + name
+// //     return output
+// // }
+
+// // console.log(hello("Chris"))
+ // console.log(hello("Dan"))
