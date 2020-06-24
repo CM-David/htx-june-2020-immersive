6131980ac2dd65401f0c7f39061fdbbf86106a86
CM-David <CM.David88@yahoo.com>
2020-06-24 10:26:44 -0500

    today

--- week04/2-Tuesday/labs/lab.js
+++ week04/2-Tuesday/labs/lab.js
@@ -1,26 +1,38 @@
 
 // Declare a variable called "firstName" whose value is your first name 
+
+var firstName = "Chris"
 // Declare a variable called "lastName" whose value is your first name 
+
+var lastName = "David"
 // Declare a variable called "name" whose value is the concatenation of firstName and lastName
 
-// Print name to the console
+var name = (firstName + lastName)
 
+// Print name to the console
+console.log(name)
 
 // Declare a variable called a with a value of 2 
 
+var a = 2
+
 //Declare a variable called b witha value of 3 
+var b = 3
 
 // Declare a variable x whose value is a + b 
-
+var x =(a + b)
 // Print x  to the screen 
-
+console.log(x);
 // Add the string "5" to x and print the result
+
 
 // Print the number of characters in firstName
 
 
 // If else, fizz buzz
-
+for (var i = 1; i < 101; i++){
+    if (i % 15 == 0)
+}
 // ternary operator
 
 
